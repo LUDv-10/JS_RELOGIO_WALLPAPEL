90b545a52234c4f547be07965ac45e468d238e1c
LUDv <144742551+LUDv-10@users.noreply.github.com>
2024-10-31 15:20:55 -0300

    v1 BETA 0.1

--- js/HORARIO.js
+++ js/HORARIO.js
@@ -7,14 +7,17 @@ function Horario() {
     document.getElementById("Time").innerHTML = "Bom dia!";
     document.getElementById("Time").style.color = "#ff9900";
     document.getElementById("Time").style.textShadow = "0vw 0vw 2vw";
+    document.getElementById("Time").style.textDecoration = "underline #ff9900";
   } else if (hora < 18) {
     document.getElementById("Time").innerHTML = "Boa tarde!";
     document.getElementById("Time").style.color = "#ffbf5e";
     document.getElementById("Time").style.textShadow = "0vw 0vw 2vw";
+    document.getElementById("Time").style.textDecoration = "underline #ffbf5e";
   } else {
     document.getElementById("Time").innerHTML = "Boa noite!";
     document.getElementById("Time").style.color = "#65c6f0";
     document.getElementById("Time").style.textShadow = "0vw 0vw 2vw";
+    document.getElementById("Time").style.textDecoration = "underline #65c6f0";
   }
 }
 
